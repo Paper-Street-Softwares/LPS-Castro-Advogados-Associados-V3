0044d10d13a27fe3970f905287db27c617b14a0d
Edison Matos <edison.matos@live.com>
2026-03-09 06:56:33 -0300

    feat: Atualiza copy

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -141,7 +141,7 @@ const content = {
       alt: "Imagem ilustrativa da Seção Início",
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
-      ctaButtonText: "Quero falar com a Advogada agora",
+      ctaButtonText: "Falar com Advogada agora",
       ctaButtonTextSecondary: "A_Definir",
       obsHero: {
         icon: <LucideIdCard />,
